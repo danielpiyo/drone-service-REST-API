8997df34229c3aebd28f0e2cf5b59dae999e8123
danielpiyo <danielopiyo8@gmail.com>
2022-12-04 14:14:22 +0300

    Created Drone and Medication repository

--- src/main/java/com/example/drone/Repository/DroneRepository.java
+++ src/main/java/com/example/drone/Repository/DroneRepository.java
@@ -1,5 +1,20 @@
 package com.example.drone.Repository;
 
-public interface DroneRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.drone.Model.Drone;
+
+public interface DroneRepository extends JpaRepository<Drone, Long>{
+	
+	List<Drone> findAll();
+	
+	List<Drone> findByState(String state);
+	
+//	Drone findBetryLevel(String serial_number);
+	
+	Optional<Drone> findByserialNumber(String serialNumber);
 
 }
